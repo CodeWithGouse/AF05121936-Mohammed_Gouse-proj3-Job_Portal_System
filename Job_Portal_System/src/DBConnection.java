01ab030f3f58405ec23c4f411ea7df9afa072739
Mohammed Gouse <mgouse539@gmail.com>
2026-04-29 22:39:16 +0530

    Update DBConnection.java

--- Job_Portal_System/src/DBConnection.java
+++ Job_Portal_System/src/DBConnection.java
@@ -6,7 +6,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/job_portal_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "sayed";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() {
         try {
